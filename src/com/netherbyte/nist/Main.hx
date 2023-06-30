17828f84a429201aca5f846acba4b7733273e191
BasicCorruption <saytleader@outlook.com>
2023-06-29 21:47:11 -0400

    Fix links

--- src/com/netherbyte/nist/Main.hx
+++ src/com/netherbyte/nist/Main.hx
@@ -5,8 +5,7 @@ class Main {
 		Server.addReplacement("domain", "localhost:3000");
 		Server.addReplacement("commonFooter", "<p>Nist is open-source software licensed under GNU GPL v3.0. <a
 					href=\" https: // github.com/netherbyte/nist\">https://github.com/netherbyte/nist</a></p>");
-		Server.addReplacement("mfIH",
-			"<button onclick='window.location.href = \"../\"'>Back</button><a href='./create.html'><button>Report Issue</button></a>");
+		Server.addReplacement("mfIH", "<button onclick='window.location.href = \"../\"'>Back</button>");
 		Server.addReplacement("mfIF", "");
 		// the header is gonna be a back button or something
 		// i can make it read from an html file
